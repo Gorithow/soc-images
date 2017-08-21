bf1c18659f689b5ee27a36274a06504440f8d6a3
Filip Tokarski <filip.tokarski@wp.pl>
2017-08-21 08:40:15 +0200

    Created Basic Web API Controller for images.

--- SocImages/ClientApp/app/components/app-root/app-root.component.ts
+++ SocImages/ClientApp/app/components/app-root/app-root.component.ts
@@ -1,4 +1,6 @@
-﻿import { Component } from '@angular/core'
+﻿import { Component, ViewChild, ElementRef } from '@angular/core'
+
+import { Http, RequestOptionsArgs } from '@angular/http';
 
 @Component({
     selector: 'app-root',
@@ -9,7 +11,52 @@ export class AppRootComponent {
     count: number = 0;
     message: string = "Hello World!";
 
-    public clicked(): void {
-        this.count++;
+    @ViewChild("fileInput") fileInput: ElementRef;
+
+    constructor(private http: Http) {}
+
+    public register(): void {
+        this.http.get('account/register', {
+            'params': {
+                'email': 'test@test.test',
+                'password': 'zaqWSX123@'
+            }
+        }).subscribe(response => alert(response));
+    }
+
+    addFile(): void {
+        let fi: HTMLInputElement = this.fileInput.nativeElement;
+        if (fi.files && fi.files[0]) {
+            let fileToUpload = fi.files[0];
+            this.upload(fileToUpload)
+                .subscribe(res => {
+                    console.log(res);
+                });
+        }
+    }
+
+    login(): void {
+        this.http.get('account/login', {
+            'params': {
+                'login': 'test@test.test',
+                'password': 'zaqWSX123@'
+            }
+        }).subscribe(response => alert(response));
+    }
+
+    image(): void {
+        this.http.get('/images/getimage', {
+            'params': {
+                'id': 1
+            }
+        }).subscribe(response => console.log(response));
+    }
+
+    upload(fileToUpload: any) {
+        let input = new FormData();
+        input.append("imageFile", fileToUpload);
+
+        return this.http
+            .post("/images/postimage", input);
     }
 }
---
+++ SocImages/Controllers/ImagesController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocImages.Data;
+using SocImages.Models;
+using System.IO;
+using SocImages.Extensions;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SocImages.Controllers
+{
+    [Produces("application/json")]
+    public class ImagesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private const string _imageContentType = "image/jpg";
+
+        public ImagesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetImage(int id)
+        {
+            var image = await _context.Images.SingleOrDefaultAsync(m => m.ImageId == id);
+
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return File(image.ImageData, _imageContentType);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> PostImage(IFormFile imageFile)
+        {
+            using (Stream stream = imageFile.OpenReadStream())
+            {
+                using (var binaryReader = new BinaryReader(stream))
+                {
+                    var fileContent = binaryReader.ReadBytes((int)imageFile.Length);
+
+                    var newImage = new Image
+                    {
+                        Title = imageFile.FileName,
+                        ImageData = fileContent,
+                        OwnerId = this.User.GetUserId()
+                    };
+
+                    _context.Add<Image>(newImage);
+
+                    await _context.SaveChangesAsync();
+
+                    return Ok();
+                }
+            }
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> VoteForImage(int id, bool voteUp = true)
+        {
+            var image = await _context.Images.SingleOrDefaultAsync(m => m.ImageId == id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            image.Rating += (voteUp ? 1 : -1);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
